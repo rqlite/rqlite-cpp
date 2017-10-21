56298d07cdd4cad9f5a605a801c5f9fee1027657
Philip O'Toole <philip.otoole@yahoo.com>
2017-10-20 22:59:52 -0700

    More abstract class implementation

--- src/rqlite.h
+++ src/rqlite.h
@@ -2,20 +2,69 @@
 #define RQLITE_H
 
 #include <string>
+#include <vector>
+
+#include <boost/any.hpp>
+// JSON parsing also available in Boost.
 
 enum ReadConsistencyLevel { 
-	kNone = 0,
-	kWeak,
-	kStrong
+    kNone = 0,
+    kWeak,
+    kStrong
 }
+
+class ExecuteResults {
+    class Result {
+        public:
+            string error;
+            int lastInsertId;
+            int rowsAffected;
+            float time;
+    };
+
+    public:
+        std::vector<Result> results;
+        float time;
+};
+
+class QueryResults {
+    class Result {
+        string error;
+        std::vector<string> columns;
+        std::vector<string> types;
+        std::vector<string> values;
+        std::vector<boost::any> values;
+        float time;
+    }
+
+    public:
+        std::vector<Result> results;
+        float time;
+};
+
+class Pong {
+    public:
+        string version;
+
+        Pong() {
+            this.version = "unknown";
+        }
+
+        Pong(string version) {
+            this.version = version;
+        }
+};
 
 class Rqlite {
 public:
-	// Query executes a single statement that returns rows.
-	QueryResults Query(string q, ReadConsistencyLevel lvl) = 0;
+    // Query executes a single statement that returns rows.
+    virtual QueryResults Query(string q, ReadConsistencyLevel lvl) = 0;
 
-	// Execute executes a single statement that does not return rows
-	ExecuteResults Execute(string q) = 0;
-};
+    // Execute executes a single statement that does not return rows
+    virtual ExecuteResults Execute(string q) = 0;
+
+    // Ping checks communication with the rqlite node. */
+    virtual Pong Ping();
+}
 
 #endif  // RQLITE_H
