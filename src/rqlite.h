e6ba03009c2f0a60227a7fe363a0fbf89a46e9a4
Philip O'Toole <philip.otoole@yahoo.com>
2017-10-21 14:24:23 -0700

    Just implement Ping() for now

--- src/rqlite.h
+++ src/rqlite.h
@@ -2,45 +2,12 @@
 #define RQLITE_H
 
 #include <string>
-#include <vector>
 
-#include <boost/any.hpp>
-// JSON parsing also available in Boost.
-
-enum ReadConsistencyLevel { 
+enum ReadConsistencyLevel {
     kNone = 0,
     kWeak,
     kStrong
 }
-
-class ExecuteResults {
-    class Result {
-        public:
-            string error;
-            int lastInsertId;
-            int rowsAffected;
-            float time;
-    };
-
-    public:
-        std::vector<Result> results;
-        float time;
-};
-
-class QueryResults {
-    class Result {
-        string error;
-        std::vector<string> columns;
-        std::vector<string> types;
-        std::vector<string> values;
-        std::vector<boost::any> values;
-        float time;
-    }
-
-    public:
-        std::vector<Result> results;
-        float time;
-};
 
 class Pong {
     public:
@@ -56,15 +23,16 @@ class Pong {
 };
 
 class Rqlite {
-public:
-    // Query executes a single statement that returns rows.
-    virtual QueryResults Query(string q, ReadConsistencyLevel lvl) = 0;
-
-    // Execute executes a single statement that does not return rows
-    virtual ExecuteResults Execute(string q) = 0;
-
-    // Ping checks communication with the rqlite node. */
-    virtual Pong Ping() = 0;
+    public:
+        // Ping checks communication with the rqlite node. */
+        virtual Pong Ping() = 0;
 }
+
+class RqliteFactory {
+    public:
+        static Rqlite Connect(string proto, string host, int port) {
+            return new RqliteImpl(proto,host, port);
+        }
+};
 
 #endif  // RQLITE_H
